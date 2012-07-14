03244705e3c771cfd267921e6f35301d152a2cda
Aaron Patterson <aaron.patterson@gmail.com>
2012-07-14 13:58:06 -0700

    renaming node to ast

--- ext/gda/gda_statement.c
+++ ext/gda/gda_statement.c
@@ -13,11 +13,10 @@ static VALUE serialize(VALUE self)
     return rb_str_new2(string);
 }
 
-static VALUE node(VALUE self)
+static VALUE ast(VALUE self)
 {
     GdaStatement * stmt;
     GdaSqlStatement * sqlst;
-    VALUE table;
 
     Data_Get_Struct(self, GdaStatement, stmt);
 
@@ -31,8 +30,7 @@ void Init_gda_statement()
     cStatement = rb_define_class_under(mSQL, "Statement", rb_cObject);
 
     rb_define_method(cStatement, "serialize", serialize, 0);
-    rb_define_method(cStatement, "node", node, 0);
-
+    rb_define_method(cStatement, "ast", ast, 0);
 }
 
 /* vim: set noet sws=4 sw=4: */
--- test/test_statement.rb
+++ test/test_statement.rb
@@ -16,59 +16,59 @@ module GDA
       end
 
       def test_from
-        assert stmt.node
-        assert stmt.node.from
+        assert stmt.ast
+        assert stmt.ast.from
       end
 
       def test_distinct_expr
-        assert_nil stmt.node.distinct_expr
+        assert_nil stmt.ast.distinct_expr
       end
 
       def test_expr_list
-        assert_equal 1, stmt.node.expr_list.length
+        assert_equal 1, stmt.ast.expr_list.length
 
-        stmt.node.expr_list.each do |node|
+        stmt.ast.expr_list.each do |node|
           assert node
         end
       end
 
       def test_where_cond
-        assert stmt.node.where_cond
+        assert stmt.ast.where_cond
       end
 
       def test_group_by
-        assert stmt.node.group_by
-        stmt.node.group_by.each do |node|
+        assert stmt.ast.group_by
+        stmt.ast.group_by.each do |node|
           assert node
         end
       end
 
       def test_having_cond
-        assert stmt.node.having_cond
+        assert stmt.ast.having_cond
       end
 
       def test_order_by
-        assert_equal 2, stmt.node.order_by.length
-        stmt.node.order_by.each do |node|
+        assert_equal 2, stmt.ast.order_by.length
+        stmt.ast.order_by.each do |node|
           assert node
         end
       end
 
       def test_limit_count
-        assert stmt.node.limit_count
+        assert stmt.ast.limit_count
       end
 
       def test_limit_offset
-        assert stmt.node.limit_offset
+        assert stmt.ast.limit_offset
       end
 
       def test_distinct?
-        refute stmt.node.distinct?
+        refute stmt.ast.distinct?
       end
 
       def test_each
         yielded = false
-        stmt.node.each do |node|
+        stmt.ast.each do |node|
           yielded = true
           assert node
         end
