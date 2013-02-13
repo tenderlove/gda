d84a58eeeb1c15686f7a2b79b05347c5eb930a08
Aaron Patterson <aaron.patterson@gmail.com>
2013-02-12 19:46:58 -0800

    updating extconf

--- ext/gda/extconf.rb
+++ ext/gda/extconf.rb
@@ -2,20 +2,15 @@ require 'mkmf'
 
 # :stopdoc:
 
-RbConfig::MAKEFILE_CONFIG['CC'] = ENV['CC'] if ENV['CC']
-
-INCLUDEDIR = RbConfig::CONFIG['includedir']
-LIBDIR     = RbConfig::CONFIG['libdir']
-LIB_DIRS   = ['/opt/local/lib', '/usr/local/lib', LIBDIR, '/usr/lib']
-libgda    = dir_config 'libgda', '/opt/local/include', '/opt/local/lib'
+dir_config 'libgda'
 
 def asplode missing
   abort "#{missing} is missing. Try 'port install libgda +universal' " +
         "or 'yum install libgda-devel'"
 end
 
-pkg_config 'libgda-4.0'
-find_header 'libgda/sql-parser/gda-sql-parser.h'
+pkg_config 'libgda-5.0'
+find_header('libgda/sql-parser/gda-sql-parser.h') || asplode("rawr")
 
 create_makefile 'gda'
 
