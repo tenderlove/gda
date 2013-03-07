3c142e70caadddc957802362ee4db23fcbd05e0c
Aaron Patterson <aaron.patterson@gmail.com>
2013-03-07 09:54:33 -0800

    fix messaging in extconf

--- ext/gda/extconf.rb
+++ ext/gda/extconf.rb
@@ -7,12 +7,11 @@ ENV['PKG_CONFIG_PATH']= '/usr/local/Library/ENV/pkgconfig/mountain_lion'
 dir_config 'libgda'
 
 def asplode missing
-  abort "#{missing} is missing. Try 'port install libgda +universal' " +
-        "or 'yum install libgda-devel'"
+  abort "#{missing} is missing. Try 'brew install libgda'"
 end
 
 pkg_config 'libgda-5.0'
-find_header('libgda/sql-parser/gda-sql-parser.h') || asplode("rawr")
+find_header('libgda/sql-parser/gda-sql-parser.h') || asplode("libgda")
 
 create_makefile 'gda'
 
