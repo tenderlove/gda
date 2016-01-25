b07e6f2518177f9d91421c3de21feaf4f84f1d13
Arthur Neves <arthurnn@gmail.com>
2016-01-25 14:45:39 -0500

    Add not equal operator test

--- test/test_nodes.rb
+++ test/test_nodes.rb
@@ -22,5 +22,10 @@ module GDA
       stmt = parser.parse 'SELECT * FROM OMG WHERE 1 = 10'
       assert_operator stmt.ast.to_a.length, :>, 1
     end
+
+    def test_not_equal_operator
+      stmt = parser.parse 'SELECT id FROM comments WHERE issue_id = 1 AND type <> 3'
+      assert stmt.ast.is_a?(GDA::Nodes::Select)
+    end
   end
 end
